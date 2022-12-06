3ec641cc8e95c1472faf21a304c6b8e852e1a324
tunmie <104634715+tunmie@users.noreply.github.com>
2022-12-06 15:12:59 +0100

    made strings resources.. building out LemonTextAndImage composable

--- app/src/main/java/dev/tunmie/lemonade/MainActivity.kt
+++ app/src/main/java/dev/tunmie/lemonade/MainActivity.kt
@@ -3,13 +3,23 @@ package dev.tunmie.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.tunmie.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +29,7 @@ class MainActivity : ComponentActivity() {
 			LemonadeTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-					Greeting("Android")
+					LemonApp()
 				}
 			}
 		}
@@ -27,17 +37,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
+fun LemonApp() {
+	Surface {
+		LemonTextAndImage()
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-	LemonadeTheme {
-		Greeting("Android")
+fun LemonTextAndImage(
+	modifier: Modifier = Modifier
+) {
+	Column(
+		horizontalAlignment = Alignment.CenterHorizontally,
+		verticalArrangement = Arrangement.Center,
+		modifier = modifier.fillMaxSize()
+	) {
+		Text(text = stringResource(id = R.string.step1), fontSize = 18.sp)
+		Spacer(modifier = Modifier.height(16.dp))
+		Image(
+			painter = painterResource(id = R.drawable.lemon_tree), contentDescription = stringResource(
+				id = R.string.title1
+			)
+		)
 	}
 }
